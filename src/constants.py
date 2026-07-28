d84333db8c6b8e9f7cb2eee75f28271a6dfef5cb
brandondia-z <brandondiaz349@gmail.com>
2026-07-27 18:12:47 -0700

    style: warm up the typography color to a buttery cream

--- src/constants.py
+++ src/constants.py
@@ -17,7 +17,7 @@ LINE_COLORS = {
 
 # Typography Colors
 AMBER = (255, 176, 0)
-WARM_YELLOW = (255, 240, 180)  # Cream color for standard text
+WARM_YELLOW = (255, 235, 100)  # Buttery cream color for standard text
 WHITE = (255, 255, 255)
 DARK_GRAY = (40, 40, 40)
 BLACK = (0, 0, 0)
